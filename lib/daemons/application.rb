ecea103d7a1763f3ed61b2c2466d95a570fee853
richo <richo@psych0tik.net>
2011-12-06 16:16:23 +1100

    Fix whitespace

--- lib/daemons/application.rb
+++ lib/daemons/application.rb
@@ -8,32 +8,32 @@ require 'timeout'
 module Daemons
 
   class Application
-  
+
     attr_accessor :app_argv
     attr_accessor :controller_argv
-    
+
     # the Pid instance belonging to this application
     attr_reader :pid
-    
+
     # the ApplicationGroup the application belongs to
     attr_reader :group
-    
+
     # my private options
     attr_reader :options
-    
-    
+
+
     SIGNAL = (RUBY_PLATFORM =~ /win32/ ? 'KILL' : 'TERM')
-    
-    
+
+
     def initialize(group, add_options = {}, pid = nil)
       @group = group
       @options = group.options.dup
       @options.update(add_options)
-      
+
       @dir_mode = @dir = @script = nil
-      
+
       @force_kill_waittime = @options[:force_kill_waittime] || 20
-      
+
       unless @pid = pid
         if @options[:no_pidfiles]
           @pid = PidMem.new
@@ -44,21 +44,21 @@ module Daemons
         end
       end
     end
-    
+
     def change_privilege
       user = options[:user]
       group = options[:group]
       CurrentProcess.change_privilege(user, group) if user
     end
-    
+
     def script
       @script || @group.script
     end
-    
+
     def pidfile_dir
       Pid.dir(@dir_mode || @group.dir_mode, @dir || @group.dir, @script || @group.script)
     end
-    
+
     def logdir
       logdir = options[:log_dir]
       unless logdir
@@ -66,15 +66,15 @@ module Daemons
       end
       logdir
     end
-    
+
     def output_logfile
       (options[:log_output] && logdir) ? File.join(logdir, @group.app_name + '.output') : nil
     end
-    
+
     def logfile
       logdir ? File.join(logdir, @group.app_name + '.log') : nil
     end
-    
+
     # this function is only used to daemonize the currently running process (Daemons.daemonize)
     def start_none
       unless options[:ontop]
@@ -82,27 +82,27 @@ module Daemons
       else
         Daemonize.simulate(output_logfile)
       end
-      
+
       @pid.pid = Process.pid
-      
-      
+
+
       # We need this to remove the pid-file if the applications exits by itself.
-      # Note that <tt>at_text</tt> will only be run if the applications exits by calling 
+      # Note that <tt>at_text</tt> will only be run if the applications exits by calling
       # <tt>exit</tt>, and not if it calls <tt>exit!</tt> (so please don't call <tt>exit!</tt>
       # in your application!
       #
       at_exit {
         begin; @pid.cleanup; rescue ::Exception; end
-        
+
         # If the option <tt>:backtrace</tt> is used and the application did exit by itself
         # create a exception log.
         if options[:backtrace] and not options[:ontop] and not $daemons_sigterm
           begin; exception_log(); rescue ::Exception; end
         end
-          
+
       }
-      
-      # This part is needed to remove the pid-file if the application is killed by 
+
+      # This part is needed to remove the pid-file if the application is killed by
       # daemons or manually by the user.
       # Note that the applications is not supposed to overwrite the signal handler for
       # 'TERM'.
@@ -110,7 +110,7 @@ module Daemons
       trap(SIGNAL) {
         begin; @pid.cleanup; rescue ::Exception; end
         $daemons_sigterm = true
-        
+
         if options[:hard_exit]
           exit!
         else
@@ -118,55 +118,55 @@ module Daemons
         end
       }
     end
-    
+
     def start_exec
       if options[:backtrace]
         puts "option :backtrace is not supported with :mode => :exec, ignoring"
       end
-      
+
       unless options[:ontop]
         Daemonize.daemonize(output_logfile, @group.app_name)
       else
         Daemonize.simulate(output_logfile)
       end
-      
+
       # note that we cannot remove the pid file if we run in :ontop mode (i.e. 'ruby ctrl_exec.rb run')
       @pid.pid = Process.pid
-      
-      ENV['DAEMONS_ARGV'] = @controller_argv.join(' ')      
+
+      ENV['DAEMONS_ARGV'] = @controller_argv.join(' ')
       # haven't tested yet if this is really passed to the exec'd process...
-      
+
       started()
       Kernel.exec(script(), *(@app_argv || []))
     end
-    
+
     def start_load
       unless options[:ontop]
         Daemonize.daemonize(output_logfile, @group.app_name)
       else
         Daemonize.simulate(output_logfile)
       end
-      
+
       @pid.pid = Process.pid
-      
-      
+
+
       # We need this to remove the pid-file if the applications exits by itself.
-      # Note that <tt>at_exit</tt> will only be run if the applications exits by calling 
+      # Note that <tt>at_exit</tt> will only be run if the applications exits by calling
       # <tt>exit</tt>, and not if it calls <tt>exit!</tt> (so please don't call <tt>exit!</tt>
       # in your application!
       #
       at_exit {
         begin; @pid.cleanup; rescue ::Exception; end
-        
+
         # If the option <tt>:backtrace</tt> is used and the application did exit by itself
         # create a exception log.
         if options[:backtrace] and not options[:ontop] and not $daemons_sigterm
           begin; exception_log(); rescue ::Exception; end
         end
-          
+
       }
-      
-      # This part is needed to remove the pid-file if the application is killed by 
+
+      # This part is needed to remove the pid-file if the application is killed by
       # daemons or manually by the user.
       # Note that the applications is not supposed to overwrite the signal handler for
       # 'TERM'.
@@ -179,35 +179,35 @@ module Daemons
         end
         rescue ::Exception
         end
-        
+
         begin; @pid.cleanup; rescue ::Exception; end
         $daemons_sigterm = true
-        
+
         if options[:hard_exit]
           exit!
         else
           exit
         end
       }
-      
+
       # Now we really start the script...
       $DAEMONS_ARGV = @controller_argv
       ENV['DAEMONS_ARGV'] = @controller_argv.join(' ')
-      
+
       ARGV.clear
       ARGV.concat @app_argv if @app_argv
-      
+
       started()
       # TODO: begin - rescue - end around this and exception logging
       load script()
     end
-    
+
     def start_proc
       return unless p = options[:proc]
-    
-      myproc = proc do 
+
+      myproc = proc do
         # We need this to remove the pid-file if the applications exits by itself.
-        # Note that <tt>at_text</tt> will only be run if the applications exits by calling 
+        # Note that <tt>at_text</tt> will only be run if the applications exits by calling
         # <tt>exit</tt>, and not if it calls <tt>exit!</tt> (so please don't call <tt>exit!</tt>
         # in your application!
         #
@@ -222,7 +222,7 @@ module Daemons
 
         }
 
-        # This part is needed to remove the pid-file if the application is killed by 
+        # This part is needed to remove the pid-file if the application is killed by
         # daemons or manually by the user.
         # Note that the applications is not supposed to overwrite the signal handler for
         # 'TERM'.
@@ -235,7 +235,7 @@ module Daemons
           end
           rescue ::Exception
           end
-          
+
           begin; @pid.cleanup; rescue ::Exception; end
           $daemons_sigterm = true
 
@@ -245,20 +245,20 @@ module Daemons
             exit
           end
         }
-        
+
         p.call()
       end
-      
+
       unless options[:ontop]
         @pid.pid = Daemonize.call_as_daemon(myproc, output_logfile, @group.app_name)
-        
+
       else
         Daemonize.simulate(output_logfile)
-        
+
         @pid.pid = Process.pid
-        
+
         myproc.call
-        
+
 # why did we use this??
 #         Thread.new(&options[:proc])
 
@@ -272,15 +272,15 @@ module Daemons
         #   Process.detach(@pid.pid)
         # end
       end
-      
+
       started()
     end
-    
-    
+
+
     def start
       change_privilege
       @group.create_monitor(@group.applications[0] || self) unless options[:ontop]  # we don't monitor applications in the foreground
-      
+
       case options[:mode]
         when :none
           # this is only used to daemonize the currently running process
@@ -295,15 +295,15 @@ module Daemons
           start_load
       end
     end
-    
+
     def started
       if pid = @pid.pid
         puts "#{self.group.app_name}: process with pid #{pid} started."
         STDOUT.flush
       end
     end
-    
-    
+
+
 #     def run
 #       if @group.controller.options[:exec]
 #         run_via_exec()
@@ -311,13 +311,13 @@ module Daemons
 #         run_via_load()
 #       end
 #     end
-#      
+#
 #     def run_via_exec
-#       
+#
 #     end
-#     
+#
 #     def run_via_load
-#       
+#
 #     end
 
 	def reload
@@ -337,44 +337,44 @@ module Daemons
     #
     def exception_log
       return unless logfile
-      
+
       require 'logger'
-      
+
       l_file = Logger.new(logfile)
-      
+
       # the code below finds the last exception
       e = nil
-      
+
       ObjectSpace.each_object {|o|
         if ::Exception === o
           e = o
         end
       }
-     
+
       l_file.info "*** below you find the most recent exception thrown, this will be likely (but not certainly) the exception that made the application exit abnormally ***"
       l_file.error e
-      
+
       l_file.info "*** below you find all exception objects found in memory, some of them may have been thrown in your application, others may just be in memory because they are standard exceptions ***"
-      
+
       # this code logs every exception found in memory
       ObjectSpace.each_object {|o|
         if ::Exception === o
           l_file.error o
         end
       }
-      
+
       l_file.close
     end
-    
-    
+
+
     def stop(no_wait = false)
       if not running?
         self.zap
         return
       end
-      
+
       pid = @pid.pid
-      
+
       # Catch errors when trying to kill a process that doesn't
       # exist. This happens when the process quits and hasn't been
       # restarted by the monitor yet. By catching the error, we allow the
@@ -385,12 +385,12 @@ module Daemons
         puts "#{e} #{pid}"
         puts "deleting pid-file."
       end
-      
+
       if not no_wait
         if @force_kill_waittime > 0
           puts "#{self.group.app_name}: trying to stop process with pid #{pid}..."
           STDOUT.flush
-          
+
           begin
             Timeout::timeout(@force_kill_waittime) {
               while Pid.running?(pid)
@@ -400,12 +400,12 @@ module Daemons
           rescue Timeout::Error
             puts "#{self.group.app_name}: process with pid #{pid} won't stop, we forcefully kill it..."
             STDOUT.flush
-            
+
             begin
               Process.kill('KILL', pid)
             rescue Errno::ESRCH
             end
-            
+
             begin
               Timeout::timeout(20) {
                 while Pid.running?(pid)
@@ -418,36 +418,36 @@ module Daemons
             end
           end
         end
-        
-        
+
+
       end
-      
+
       sleep(0.1)
       unless Pid.running?(pid)
         # We try to remove the pid-files by ourselves, in case the application
         # didn't clean it up.
         begin; @pid.cleanup; rescue ::Exception; end
-        
+
         puts "#{self.group.app_name}: process with pid #{pid} successfully stopped."
         STDOUT.flush
       end
-      
+
     end
-    
+
     def zap
       @pid.cleanup
     end
-    
+
     def zap!
       begin; @pid.cleanup; rescue ::Exception; end
     end
-    
+
     def show_status
       running = self.running?
-      
+
       puts "#{self.group.app_name}: #{running ? '' : 'not '}running#{(running and @pid.exist?) ? ' [pid ' + @pid.pid.to_s + ']' : ''}#{(@pid.exist? and not running) ? ' (but pid-file exists: ' + @pid.pid.to_s + ')' : ''}"
     end
-    
+
     # This function implements a (probably too simle) method to detect
     # whether the program with the pid found in the pid-file is still running.
     # It just searches for the pid in the output of <tt>ps ax</tt>, which
@@ -459,9 +459,9 @@ module Daemons
       if @pid.exist?
         return Pid.running?(@pid.pid)
       end
-      
+
       return false
     end
   end
-  
+
 end
